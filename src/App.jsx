b5e85b5aa1aa17f8d5a54d2266c2530be4a64997
KaueSiqueira <kaue54150@gmail.com>
2026-04-24 18:24:49 -0300

    Funcionalidades finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Section_funcionalidades from "./components/Section_funcionalidades";
 
 function App() {
   return (
     <main>
       <Header />
       <Hero />
+      <Section_funcionalidades />
     </main>
   );
 }
---
+++ src/components/Section_funcionalidades.jsx
@@ -0,0 +1,44 @@
+import styles from "../css/Section_funcionalidades.module.css";
+import Video from "../assets/Funcionalidades/Video.mp4";
+import Funcionalidade_1 from "../assets/Funcionalidades/Speaker.svg";
+import Funcionalidade_2 from "../assets/Funcionalidades/Speaker2.png";
+import Funcionalidade_3 from "../assets/Funcionalidades/hand.svg";
+
+function Section_funcionalidades() {
+  return (
+    <section className={styles.sec_funcionalidades}>
+      <div className={styles.video_f}>
+        <video autoPlay loop muted>
+          <source src={Video} />
+        </video>
+      </div>
+      <div className={styles.funcionalidades}>
+        <div className={styles.container_itens}>
+          <h1>Funcionalidades do sistema</h1>
+          <div className={styles.container_func}>
+            <div className={styles.func}>
+              <div className={styles.imagem_func}>
+                <img src={Funcionalidade_1} alt="" />
+              </div>
+              <h3>Tradução em voz/texto para Libras</h3>
+            </div>
+            <div className={styles.func}>
+              <div className={styles.imagem_func}>
+                <img src={Funcionalidade_2} alt="" />
+              </div>
+              <h3>Tradução de Libras para aúdio/texto</h3>
+            </div>
+            <div className={styles.func}>
+              <div className={styles.imagem_func}>
+                <img src={Funcionalidade_3} alt="" />
+              </div>
+              <h3>Leitor de mãos ambidestro</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Section_funcionalidades;
