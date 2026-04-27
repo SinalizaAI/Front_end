aef8e04755e47c1f52d6010682f0622458bca923
KaueSiqueira <kaue54150@gmail.com>
2026-04-27 17:17:17 -0300

    Tradutor - inicio

--- src/components/Tradutor.jsx
+++ src/components/Tradutor.jsx
@@ -3,11 +3,10 @@ import styles from "../css/Tradutor.module.css";
 function Tradutor() {
   return (
     <section className={styles.sec}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, et
-        cumque? Voluptate odit aut ipsum porro quibusdam. Iure distinctio id,
-        sint culpa veritatis porro maxime quas. A nam voluptatibus fuga.
-      </p>
+      <h1>Seja bem vindo!</h1>
+      <div className={styles.container}>
+        <button>Audio/Texto para libras</button>
+      </div>
     </section>
   );
 }
