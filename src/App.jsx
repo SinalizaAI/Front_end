441c6202121839c87207e85c330c9113e87c14dc
KaueSiqueira <kaue54150@gmail.com>
2026-04-27 21:41:09 -0300

    sobre equipe - inicio

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Tradutor from "./pages/Tradutor";
 import PlanosCompletos from "./pages/PlanosCompletos";
+import Sobre_Equipe from "./pages/Sobre_Equipe";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Tradutor" element={<Tradutor />} />
           <Route path="/PlanosCompletos" element={<PlanosCompletos />} />
+          <Route path="/pages/Sobre_Equipe" element={<Sobre_Equipe />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Sobre_Equipe.jsx
+++ src/pages/Sobre_Equipe.jsx
@@ -1,7 +1,39 @@
 import styles from "../css/Sobre_Equipe.module.css";
+import Equipe from "../assets/Sobre_Equipe/equipe.png";
+import Kaue from "../assets/Sobre_Equipe/Kaue2.png";
 
 function Sobre_Equipe() {
-  return <></>;
+  return (
+    <section className={styles.sec}>
+      <div className={styles.image_equipe}>
+        <img src={Equipe} alt="" />
+      </div>
+      <div className={styles.text}>
+        <h2>
+          Somos proanos apaixonados por tecnologia, movidos pelo entusiamo e
+          propósito de tornar a comunicação mais acessível. Cada integrante
+          contribui com conhecimentos e habilidades únicas no desenvolvimento do
+          nosso software, unindo conhecimento técnico, criatividade, trabalho em
+          equipe e empatia.
+        </h2>
+      </div>
+
+      <div className={styles.person_cont}>
+        <div className={styles.name}>
+          <div className={styles.container}>
+            <h1>Kauê Siqueira</h1>
+            <h3>Front-end e UI/UX</h3>
+            <div className={styles.hab}>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.foto}>
+          <img src={Kaue} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Sobre_Equipe;
